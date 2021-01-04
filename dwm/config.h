3c405914862a3f948b522ae23dc230489b039957
lesacar <69954403+lesacar@users.noreply.github.com>
2021-01-04 15:02:14 +0100

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -65,7 +65,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browser[]  = { "brave", NULL };
-static const char *sclocker[]  = { "slock", NULL };
+static const char *sclocker[]  = { "xlock", NULL };
 static const char *scrotcmd[]  = { "scrot", "-t", "25", "/home/altra/screenshots/img", NULL };
 static const char *scrotfocusedcmd[]  = { "scrot", "--focused", NULL };
 
